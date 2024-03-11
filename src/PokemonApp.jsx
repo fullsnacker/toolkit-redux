186879e41e9910256954156e75edfbf447c06605
fullsnacker <fullsnacker@gmail.com>
2024-03-11 08:53:18 -0300

    snippet and new slice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,13 @@
+export const PokemonApp = () => {
+	return (
+		<>
+			<h1>PokemonApp</h1>
+			<hr />
+			<ul>
+				<li>Hola</li>
+				<li>Hola</li>
+				<li>Hola</li>
+			</ul>
+		</>
+	);
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { PokemonApp } from './PokemonApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			{/* <App /> */}
+			<PokemonApp />
 		</Provider>
 	</React.StrictMode>
 );
